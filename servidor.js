52c6a36570ee816ccee96b4a14f9f193a9b92ba3
Marcelo <marcelogvz27@gmail.com>
2026-02-11 05:27:18 -0300

    middleware, rutas de prueba y escuchando el puerto para que se pueda correr el servidor

--- servidor.js
+++ servidor.js
@@ -3,3 +3,17 @@ const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
+
+//middleware
+app.use(cors()); //permite request desde otro puerto 
+app.use(express.json()); //para recibir json en post
+
+//ruta de prueba 
+app.get("/", (req, res) =>{
+    res.send("servidor corriendo");
+});
+
+//iniciar servidor
+app.listen(PORT, () =>{
+    console.log("servidor corriendo en http://localhost:${PORT}");
+});
