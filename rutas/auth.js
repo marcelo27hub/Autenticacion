983eaedfabbf5d6a1c3f24c1c5e175b7993074e1
Marcelo <marcelogvz27@gmail.com>
2026-02-11 05:52:50 -0300

    login creado

--- rutas/auth.js
+++ rutas/auth.js
@@ -1,6 +1,7 @@
 //arrays para guardarlos temporalmente
 const usuarios =[];
 
+//ruta de registro
 app.post("/register", (req, res) =>{
     const {email, password} = req.body;
 
@@ -17,5 +18,24 @@ app.post("/register", (req, res) =>{
 
     usuarios.push({email, password});
 
-    res.status(200).json({mensaje: "usuario registrado correctamente"});
+    res.status(201).json({mensaje: "usuario registrado correctamente"});
+});
+
+//ruta de login 
+app.post("/login", (req,res) =>{
+    const {email, password} = req.body;
+
+        //verificar que los datos existen 
+    if (!email || !password) {
+        return res.status(400).json({mensaje: "Faltan datos"})
+    }
+
+    //buscamos el usuario
+    const usuario = usuarios.find(u => u.email === email && u.password === password);
+
+    if (!usuario){
+        return res.status(401).json({mensaje: "usuario om contrasenha incorrecta"});
+    }
+
+    res.status(200).json({mensaje: "login exitoso"});
 });
