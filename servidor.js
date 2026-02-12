dccd2f68b376efc9322bd2931b376a8be79e702a
Marcelo <marcelogvz27@gmail.com>
2026-02-11 21:11:30 -0300

    traemos las rutas al servidor

--- servidor.js
+++ servidor.js
@@ -1,5 +1,8 @@
+//servidor
 const express = require("express");
 const cors = require("cors");
+
+const authrutas = require("./rutas/auth")
 
 const app = express();
 const PORT = 5000;
@@ -8,10 +11,15 @@ const PORT = 5000;
 app.use(cors()); //permite request desde otro puerto 
 app.use(express.json()); //para recibir json en post
 
+//rutas
+app.use("/auth", authrutas)
+
+
 //ruta de prueba 
 app.get("/", (req, res) =>{
     res.send("servidor corriendo");
 });
+
 
 //iniciar servidor
 app.listen(PORT, () =>{
