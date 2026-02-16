4228be60058029e7a0754e34b430828c1207fdcb
Marcelo <marcelogvz27@gmail.com>
2026-02-16 03:18:59 -0300

    cambios de variables

--- rutas/auth.js
+++ rutas/auth.js
@@ -12,11 +12,11 @@ router.post("/register", async (req, res) =>{
 
     const hash = await bcrypt.hash(password, 10); //para hashear la contrassenha
 
-    db.run("INSERT INTO usuarios (email, password) VALUES(?, ?)",
+    db.run("INSERT INTO users (email, password) VALUES(?, ?)",
         [email,hash],
         function (error){
             if (error){
-                return res.status(400).json({mensaje: "Usuario ya registrado"});
+                return res.status(400).json({mensaje: "usuario ya registrado"});
             } 
             return res.status(201).json({mensaje: "usuario registrado correctamente"});
         }
@@ -30,12 +30,12 @@ router.post("/login", async (req,res) =>{
     //verificar que los datos existen 
     if (!email || !password)  return res.status(400).json({mensaje: "Faltan datos"})
     
-    db.get("SELECT * FROM usuarios WHERE email = ?", [email], async (error, usuario) =>{
+    db.get("SELECT * FROM users WHERE email = ?", [email], async (error, user) =>{
         if (error) return res.status(500).json({mensaje: "error interno"});
-        if (!usuario) return res.status(401).json({mensaje: "usuario o contrasenha incorrecta"});
+        if (!user) return res.status(401).json({mensaje: "usuario o contrasenha incorrecta"});
         
         //comparamos los hashes\
-        const coincide = await bcrypt.compare(password, usuario.password);
+        const coincide = await bcrypt.compare(password, user.password);
         if (!coincide) return res.status(401).json({mensaje: "usuario o contrasenha incorrecta"});
 
         res.status(200).json({mensaje: "Login exitoso"});
