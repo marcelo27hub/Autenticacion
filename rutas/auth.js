dd9f6ac25e185f9cc0fc8062b2e878e75842a7aa
Marcelo <marcelogvz27@gmail.com>
2026-03-12 14:11:08 -0300

    rutas listas, me equivoque de archivo eh hice controladores aqui!

--- rutas/auth.js
+++ rutas/auth.js
@@ -1,125 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const db = require("../config/database");
-
-
-const JWT_SECRET = "clave_jwt_secreta";
-
-
-//ruta de registro
-router.post("/register", async (req, res) =>{
-    const {email, password} = req.body;
-
-    //verificar que los datos existen 
-    if (!email || !password) return res.status(400).json({mensaje: "Faltan datos"})
-
-    const hash = await bcrypt.hash(password, 10); //para hashear la contrassenha
-
-    db.run("INSERT INTO users (email, password) VALUES(?, ?)",
-        [email,hash],
-        function (error){
-            if (error){
-                return res.status(400).json({mensaje: "usuario ya registrado"});
-            } 
-            return res.status(201).json({mensaje: "usuario registrado correctamente"});
-        }
-    );    
-});
-
-//login con (cookie o jwt)
-router.post("/login", async (req,res) =>{
-    const {email, password, authType} = req.body;
-
-    //verificar que los datos existen 
-    if (!email || !password)  return res.status(400).json({mensaje: "Faltan datos"})
+const authController = require("../controllers/authController");
+const verificarJWT = require("../middlewares/jwtMiddleware");
+const verificarRol = require("../middlewares/rolMiddleware");
+module.exports = ( loginLimiter) => {
     
-    db.get("SELECT * FROM users WHERE email = ?", [email], async (error, user) =>{
-        if (error) return res.status(500).json({mensaje: "error interno"});
-        if (!user) return res.status(401).json({mensaje: "usuario o contrasenha incorrecta"});
-        
-        //comparamos los hashes\
-        const coincide = await bcrypt.compare(password, user.password);
-        if (!coincide) return res.status(401).json({mensaje: "usuario o contrasenha incorrecta"});
+    router.post("/register", authController.register);
+    router.post("/login", loginLimiter, authController.login);
+    router.get("/perfil", authController.perfilCookie);
+    router.post("/logout", authController.logout);
+    router.get("/perfil-jwt", verificarJWT, authController.perfilJWT);
 
-
-        //opcion cookie 
-        if (authType  === "cookie"){
-            req.session.userId = user.id;
-            req.session.role = user.rol;
-
-            return res.json({mensaje: "login con cookie exitoso"});
-        }
-
-        //opcion jwt
-        if (authType === "jwt"){
-            const token = jwt.sign({
-                id: user.id, role: user.rol},
-                JWT_SECRET,
-                {expiresIn: "1h"}
-
-            );
-            return res.json({
-                mensaje: "login con jwt extiso", token
-            });
-        }
-        res.status(400).json({mensaje: "Tipo de autenticacion invalida"});
-
-    });
-});
-
-
-//perfil con cookie
-router.get("/perfil", (req,res) =>{
-    if (!req.session.userId){
-        return res.status(401).json({mensaje: "No autenticado"});
-    }
-
-    res.json({
-        mensaje: "Perfil con cookie",
-        userId: req.session.userId,
-        role: req.session.role
-    });
-});
-
-
-//logout cookie
-router.post("/logout", (req, res) =>{
-    req.session.destroy(() => {
-        res.clearCookie("connect.sid")
-        res.json({mensaje: "sesion cerrada"});
-    });
-});
-
-
-//middleware jwt
-function verificarjwt(req,res,next) {
-    const authHeader = req.headers.auhorization;
-    if(!authHeader){
-        return res.status(401).json({mensaje: "Token requerido"});
-    }
-    
-    const token = authHeader.split(" ")[1];
-
-    jwt.verify(token, JWT_SECRET, (error, decoded) => {
-        if (error) {
-            return res.status(401).json({mensaje: "Token invalido"});
-        }
-
-        req.user = decoded;
-        next();
-    });
-    
-}
-
-
-//pperfil con jwt 
-router.get("/perfil-jwt", verificarjwt, (req, res) =>{
-    res.json({
-        mensaje: " perfil con jwt",
-        user: req.user
-    });
-});
-
-module.exports = router;
+    // Dashboard de administrador
+    router.get("/admin-dashboard", verificarJWT, verificarRol("administrador"), authController.dashboard);
+    return router
+};
