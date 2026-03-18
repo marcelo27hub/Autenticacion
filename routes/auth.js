2307e114db6710dd1f4c5e3b7523830d3f42d068
Marcelo <marcelogvz27@gmail.com>
2026-03-18 04:05:34 -0300

    mis rutas listas

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authcontrollers");
+const verificarJWT = require("../middlewares/jwtMiddleware");
+const verificarRol = require("../middlewares/rolMiddleware");
+const verificarSesionOCookie = require("../middlewares/verificarsesion0cookie");
+module.exports = ( loginLimiter) => {
+
+    // Formularios GET
+    router.get("/login", authController.mostrarLogin);
+    router.get("/register", authController.mostrarRegister);
+
+    //formularios POST
+    router.post("/login", loginLimiter, authController.login);
+    router.post("/register", authController.register);
+    router.post("/logout", authController.logout);
+
+    // perfil
+    router.get("/perfil", authController.mostrarPerfil)
+    router.get("/perfil-jwt", verificarJWT, authController.perfilJWT);
+
+    // Dashboard de administrador
+    router.get("/admin-dashboard", verificarSesionOCookie, verificarRol("administrador"), authController.adminDashboard);
+    return router;
+};
